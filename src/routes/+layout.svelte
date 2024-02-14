462228c974d52ef66448eaf6463bfb9769130856
Luke <luke@larval.co.uk>
2024-02-14 18:57:25 +1300

    Setup SvelteKit project and Cloudflare adaptor

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import "../app.pcss";
+    import {AppBar, AppLayout, Button, Menu, MenuItem, NavItem, Settings, Toggle} from 'svelte-ux';
+    import { page } from "$app/stores";
+    import {mdiAccount, mdiFileMultiple, mdiHome} from "@mdi/js";
+</script>
+
+<AppLayout>
+    <nav slot="nav" class="h-full">
+        <NavItem text="Home" icon={mdiHome} currentUrl={$page.url} path="/" />
+        <NavItem text="Files" icon={mdiFileMultiple} currentUrl={$page.url} path="/files" />
+    </nav>
+
+    <AppBar title="Bungy" class="bg-primary" menuIcon={null}>
+        <div slot="actions">
+            <Settings classes={{Button: "border-2 font-bold", Menu: "shadow-xl border-gray-500", MenuItem: "font-bold"}}>
+                <Toggle let:on={open} let:toggle>
+                    <Button on:click={toggle} icon={mdiAccount}>
+                        Luke
+                        <Menu {open} on:close={toggle}>
+                            <MenuItem href="/account">Manage Account</MenuItem>
+                        </Menu>
+                    </Button>
+                </Toggle>
+            </Settings>
+        </div>
+    </AppBar>
+
+    <main>
+        <slot />
+    </main>
+</AppLayout>
